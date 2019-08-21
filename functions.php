4d13836889c4375ed7af2df0dd84eb3206693c71
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-21 12:44:29 +0600

    Change Title priority In Single Page

--- functions.php
+++ functions.php
@@ -573,3 +573,12 @@ function twentysixteen_widget_tag_cloud_args( $args ) {
 	return $args;
 }
 add_filter( 'widget_tag_cloud_args', 'twentysixteen_widget_tag_cloud_args' );
+
+
+
+
+//Remove Title 
+remove_action("woocommerce_single_product_summary", "woocommerce_template_single_title", 5);
+
+//Change Priority Shedule
+add_action("woocommerce_single_product_summary", "woocommerce_template_single_title", 35);

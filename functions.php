abfc9581d8b9ae455efe9018569e43d05fd6a200
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-21 19:04:33 +0600

    Exclude products from a particular category on the shop page

--- functions.php
+++ functions.php
@@ -688,3 +688,22 @@ if (!function_exists('loop_columns')) {
 
 
 
+/**
+ * Exclude products from a particular category on the shop page
+ */
+function custom_pre_get_posts_query( $q ) {
+
+    $tax_query = (array) $q->get( 'tax_query' );
+
+    $tax_query[] = array(
+           'taxonomy' => 'product_cat',
+           'field' => 'slug',
+           'terms' => array( 'clothing' ), // Don't display products in the clothing category on the shop page.
+           'operator' => 'NOT IN'
+    );
+
+
+    $q->set( 'tax_query', $tax_query );
+
+}
+add_action( 'woocommerce_product_query', 'custom_pre_get_posts_query' );  
